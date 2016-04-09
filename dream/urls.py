d94843d2165d207c1f9f19cf8d0cf7e1eecee53c
Gyubin Son <geubin0414@gmail.com>
2016-04-09 23:29:16 +0900

    local views.py 내용 추가

--- dream/urls.py
+++ dream/urls.py
@@ -2,15 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # main
     url(r'^$', views.index, name='index'),
-    url(r'^1$', views.result1, name='result1'),
-    url(r'^2$', views.result2, name='result2'),
-    url(r'^3$', views.result3, name='result3'),
-    url(r'^4$', views.result4, name='result4'),
-    url(r'^get_data_1$', views.get_data_1, name='get_data_1'),
-    url(r'^get_data_2$', views.get_data_2, name='get_data_2'),
-    url(r'^get_data_3$', views.get_data_3, name='get_data_3'),
-    url(r'^get_data_4$', views.get_data_4, name='get_data_4'),
+
+    # project popup
     url(r'^bukchon$', views.bukchon, name='bukchon'),
     url(r'^changdeokgung$', views.changdeokgung, name='changdeokgung'),
     url(r'^insadong$', views.insadong, name='insadong'),
@@ -18,4 +13,47 @@ urlpatterns = [
     url(r'^n_seoul_tower$', views.n_seoul_tower, name='n_seoul_tower'),
     url(r'^war_memorial_korea$', views.war_memorial_korea, name='war_memorial_korea'),
     url(r'^myeongdong$', views.myeongdong, name='myeongdong'),
+
+    #################################################################
+    # page
+    # pages about positive, negative
+    url(r'^show_pos_bukchon$', views.show_pos_bukchon, name='show_pos_bukchon'),
+    url(r'^show_pos_changdeokgung$', views.show_pos_changdeokgung, name='show_pos_changdeokgung'),
+    url(r'^show_pos_insadong$', views.show_pos_insadong, name='show_pos_insadong'),
+    url(r'^show_pos_myeongdong$', views.show_pos_myeongdong, name='show_pos_myeongdong'),
+    url(r'^show_pos_gbp$', views.show_pos_gbp, name='show_pos_gbp'),
+    url(r'^show_pos_ntower$', views.show_pos_ntower, name='show_pos_ntower'),
+    url(r'^show_pos_war_memorial$', views.show_pos_war_memorial, name='show_pos_war_memorial'),
+
+    # pages about weighted relation
+    url(r'^show_weighted_relation_bc$', views.show_weighted_relation_bc, name='show_weighted_relation_bc'),
+    url(r'^show_weighted_relation_changdeok$', views.show_weighted_relation_changdeok, name='show_weighted_relation_changdeok'),
+    url(r'^show_weighted_relation_gbp$', views.show_weighted_relation_gbp, name='show_weighted_relation_gbp'),
+    url(r'^show_weighted_relation_insa$', views.show_weighted_relation_insa, name='show_weighted_relation_insa'),
+    url(r'^show_weighted_relation_md$', views.show_weighted_relation_md, name='show_weighted_relation_md'),
+    url(r'^show_weighted_relation_ntower$', views.show_weighted_relation_ntower, name='show_weighted_relation_ntower'),
+    url(r'^show_weighted_relation_war$', views.show_weighted_relation_war, name='show_weighted_relation_war'),
+
+    #################################################################
+    ## data
+    # data about positive, negative
+    url(r'^get_data_pos_bukchon$', views.get_data_pos_bukchon, name='get_data_pos_bukchon'),
+    url(r'^get_data_pos_changdeokgung$', views.get_data_pos_changdeokgung, name='get_data_pos_changdeokgung'),
+    url(r'^get_data_pos_insadong$', views.get_data_pos_insadong, name='get_data_pos_insadong'),
+    url(r'^get_data_pos_myeongdong$', views.get_data_pos_myeongdong, name='get_data_pos_myeongdong'),
+    url(r'^get_data_pos_gbp$', views.get_data_pos_gbp, name='get_data_pos_gbp'),
+    url(r'^get_data_pos_ntower$', views.get_data_pos_ntower, name='get_data_pos_ntower'),
+    url(r'^get_data_pos_war_memorial$', views.get_data_pos_war_memorial, name='get_data_pos_war_memorial'),
+
+    # data about weighted relation
+    url(r'^get_data_weighted_relation_bc$', views.get_data_weighted_relation_bc, name='get_data_weighted_relation_bc'),
+    url(r'^get_data_weighted_relation_changdeok$', views.get_data_weighted_relation_changdeok, name='get_data_weighted_relation_changdeok'),
+    url(r'^get_data_weighted_relation_gbp$', views.get_data_weighted_relation_gbp, name='get_data_weighted_relation_gbp'),
+    url(r'^get_data_weighted_relation_insa$', views.get_data_weighted_relation_insa, name='get_data_weighted_relation_insa'),
+    url(r'^get_data_weighted_relation_md$', views.get_data_weighted_relation_md, name='get_data_weighted_relation_md'),
+    url(r'^get_data_weighted_relation_ntower$', views.get_data_weighted_relation_ntower, name='get_data_weighted_relation_ntower'),
+    url(r'^get_data_weighted_relation_war$', views.get_data_weighted_relation_war, name='get_data_weighted_relation_war'),
+
+    # data about number of reviews
+    url(r'^get_data_4$', views.get_data_4, name='get_data_4'),
 ]
--- dream/views.py
+++ dream/views.py
@@ -27,34 +27,77 @@ def result1(request):
     return render(request, 'dream/result1.html', {})
 def result2(request):
     return render(request, 'dream/result2_1.html', {})
-
 def result3(request):
     return render(request, 'dream/result3.html', {})
 def result4(request):
     return render(request, 'dream/result4.html')
 
 # get all 35 datas.
-def get_data_1(request):
-    with open('dream/static/data/relation/data_bukchon.json') as data_file:
+# get positive, negative data
+def get_data_pos_bukchon(request):
+    with open('dream/static/data/pos_neg/pos_ngv_bukchon.json') as data_file:
         data = json.load(data_file)
     return JsonResponse(data)
-def get_data_2(request):
-    with open('dream/static/data/file_name2.json') as data_file:
+def get_data_pos_changdeokgung(request):
+    with open('dream/static/data/pos_neg/pos_neg_changdeokgung.json') as data_file:
         data = json.load(data_file)
     return JsonResponse(data)
-
-def get_data_3(request):
-    #pos_neg_changdeokgung
-    #pos_neg_insadong
-    #pos_neg_myeongdong
-    #pos_neg_tripadvisor_gbp
-    #pos_ngv_bukchon
-    #pos_ngv_n_seoul_tower
-    #pos_ngv_the_war_memorial_of_korea
+def get_data_pos_insadong(request):
+    with open('dream/static/data/pos_neg/pos_neg_insadong.json') as data_file:
+        data = json.load(data_file)
+    return JsonResponse(data)
+def get_data_pos_myeongdong(request):
+    with open('dream/static/data/pos_neg/pos_neg_myeongdong.json') as data_file:
+        data = json.load(data_file)
+    return JsonResponse(data)
+def get_data_pos_gbp(request):
+    with open('dream/static/data/pos_neg/pos_neg_tripadvisor_gbp.json') as data_file:
+        data = json.load(data_file)
+    return JsonResponse(data)
+def get_data_pos_ntower(request):
+    with open('dream/static/data/pos_neg/pos_ngv_n_seoul_tower.json') as data_file:
+        data = json.load(data_file)
+    return JsonResponse(data)
+def get_data_pos_war_memorial(request):
     with open('dream/static/data/pos_neg/pos_ngv_the_war_memorial_of_korea.json') as data_file:
         data = json.load(data_file)
     return JsonResponse(data)
 
+# get weighted relation data
+def get_data_weighted_relation_bc(request):
+    with open('dream/static/data/relation/data_bukchon.json') as data_file:
+        data = json.load(data_file)
+    return JsonResponse(data)
+def get_data_weighted_relation_changdeok(request):
+    with open('dream/static/data/relation/data_cdp.json') as data_file:
+        data = json.load(data_file)
+    return JsonResponse(data)
+def get_data_weighted_relation_gbp(request):
+    with open('dream/static/data/relation/data_gbp.json') as data_file:
+        data = json.load(data_file)
+    return JsonResponse(data)
+def get_data_weighted_relation_insa(request):
+    with open('dream/static/data/relation/data_insadong.json') as data_file:
+        data = json.load(data_file)
+    return JsonResponse(data)
+def get_data_weighted_relation_md(request):
+    with open('dream/static/data/relation/data_myeongdong.json') as data_file:
+        data = json.load(data_file)
+    return JsonResponse(data)
+def get_data_weighted_relation_ntower(request):
+    with open('dream/static/data/relation/data_ntower.json') as data_file:
+        data = json.load(data_file)
+    return JsonResponse(data)
+def get_data_weighted_relation_war(request):
+    with open('dream/static/data/relation/data_warmuseum.json') as data_file:
+        data = json.load(data_file)
+    return JsonResponse(data)
+
+# get review count
+
+# get specified relation data
+
+# get word distribution
 def get_data_4(request):
     with open('dream/static/data/flare.json') as data_file:
         data = json.load(data_file)
